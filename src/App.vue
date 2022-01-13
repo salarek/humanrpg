d472b61adc6ebe54dd1cda7ad6ca41f65c26436e
Paweł Sala <salarek15@wp.pl>
2022-01-13 22:36:57 +0100

    base configuration

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<style lang="scss">
+html,
+body {
+  // height: 100%;
+  // width: 100%;
+  overscroll-behavior: contain;
+  background: #f2f2f2;
 }
-</script>
 
-<style>
 #app {
+  overscroll-behavior: contain;
+  user-select: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  height: 100%;
+  width: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <header class="header-bar"></header>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  background-color: rgba($color: #67fff7, $alpha: 1);
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+}
+</style>
