f364939b35cf6ffc4c6b6c45d3c168116f4748e5
Paweł Sala <salarek15@wp.pl>
2022-01-14 22:35:58 +0100

    login

---
+++ src/components/login.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="login">
+    <div v-if="mode == 'login'">
+      <div class="mb-3 row">
+        <p>Logowanie</p>
+        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="staticEmail" />
+        </div>
+      </div>
+      <div class="mb-3 row">
+        <label for="inputPassword" class="col-sm-2 col-form-label"
+          >Password</label
+        >
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword" />
+        </div>
+      </div>
+    </div>
+
+    <div v-if="mode == 'registration'">
+      <div class="mb-3 row">
+        <p>Rejestracja</p>
+        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="staticEmail" />
+        </div>
+      </div>
+      <div class="mb-3 row">
+        <label for="inputPassword" class="col-sm-2 col-form-label"
+          >Password</label
+        >
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword" />
+        </div>
+      </div>
+    </div>
+    <div class="col-auto">
+      <button
+        type="submit"
+        class="btn btn-primary mb-3 m-2"
+        @click="mode = 'login'"
+      >
+        Zaloguj sie
+      </button>
+      <button
+        type="submit"
+        class="btn btn-primary mb-3 m-2"
+        @click="mode = 'registration'"
+      >
+        Rejestracja
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      mode: "login",
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.login {
+  position: relative;
+  padding: 4%;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  border-radius: 24px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,36 +2,57 @@
   <div class="home">
     <header class="header-bar">
       <div style="padding: 12px" class="bg-primary text-white">Human RPG</div>
-      <button type="button" class="btn btn-light">Light</button>
     </header>
+    <div class="main-view">
+      <div class="greetings-container">hello</div>
+      <div class="login-form"><login /></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
+import login from "@/components/login.vue";
 
 export default {
   name: "Home",
-  components: {},
+  components: { login },
 };
 </script>
 <style lang="scss" scoped>
 @import "@/scss/variables";
 .home {
-  background-color: rgba($color: #67fff7, $alpha: 1);
+  position: absolute;
+  background-color: $primary;
   width: 100vw;
   height: 100vh;
-  display: flex;
-  flex-direction: row;
+
   justify-content: center;
 }
 .header-bar {
-  background-color: #7e7e7e;
-
+  background-color: $secondary;
   width: 100%;
   height: 10vh;
   display: flex;
   flex-direction: row;
+}
+.main-view {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.greetings-container {
+  padding: 2%;
+  min-width: 300px;
+
+  width: 50%;
+}
+.login-form {
+  position: relative;
+  padding: 2%;
+  min-width: 300px;
+
+  width: 50%;
 }
 </style>
